ff35aa2eaddfe928659a18394777a305b232ea4a
Mays Jones <maysjones2@gmail.com>
2022-03-21 17:56:43 -0400

    commit tree e21b04eab2d06d1529b1dc1efed499a4c73622bf

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(x ='roller-skate'){
+return `This Saturday, I want to ${x}!`
+}
+
+function mondayWork(c='go to the office'){
+    return `This Monday, I will ${ c }.`
+}
+
+function wrapAdjective(v){
+    return function innerF(b="special"){
+        return `You are ${v}${b}${v}!`
+    }}
